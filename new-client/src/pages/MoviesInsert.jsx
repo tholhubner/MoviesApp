b3d62a76d573f9ef65e1f7e569f83f4338af0d86
Tyler Holhubner <tholhubner@gmail.com>
2023-04-06 04:23:07 -0400

    started create movie screen, title and rating slider completE

--- new-client/src/pages/MoviesInsert.jsx
+++ new-client/src/pages/MoviesInsert.jsx
@@ -1,13 +1,35 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 
-class MoviesInsert extends Component {
-	render() {
-		return (
-			<div>
-				<p>Here you will add movies</p>
+const MoviesInsert = () => {
+	const [rating, setRating] = useState(0)
+	return (
+		<div className="form-control w-1/2">
+			<label>Movie Name</label>
+			<input type="text" className="input input-bordered mb-5" />
+			<label>Rating</label>
+			<input
+				type="range"
+				min="0"
+				max="10"
+				step="0.1"
+				value={rating}
+				className="range range-lg"
+				onChange={(event) => setRating(event.target.value)}
+			/>
+			<div className="w-full flex justify-between text-xs px-2">
+				<span>|</span>
+				<span>|</span>
+				<span>|</span>
+				<span>|</span>
+				<span>|</span>
+				<span>|</span>
+				<span>|</span>
+				<span>|</span>
+				<span>|</span>
+				<span>|</span>
 			</div>
-		)
-	}
+		</div>
+	)
 }
 
 export default MoviesInsert
